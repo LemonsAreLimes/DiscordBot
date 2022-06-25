2176b41b833d93eee1b265b861fff2328bef625c
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-25 12:50:54 -0600

    testing out cogs

--- cogs/admin_tools.py
+++ cogs/admin_tools.py
@@ -1,13 +1,17 @@
 import discord
-import discord.ext.commands as commands
-from discord import message
+import discord.ext.commands
 
-client = commands.Bot(command_prefix="rc.admin.")
+client = discord.ext.commands.Bot(command_prefix="rc.")
 
-class admin_Tools(commands.Cog):
+class admin_Tools(discord.ext.commands.Cog):
     def __init__(self, client):
         self.client = client
-    print("Cog_loaded: admin_tools.py")
+    print("admin_tools initialized")
+
+    @client.event()
+    async def on_ready():
+        print('admin_tools loaded!')
+
 
     @client.command()
     async def test(ctx):
