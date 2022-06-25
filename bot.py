8740019b41277d2b48ee7ef3def66293db160a37
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-24 20:28:39 -0600

    bot should now connect to discord

--- bot.py
+++ bot.py
@@ -1,30 +1,30 @@
-#alot of this is coming from a previous attempt of a discord bot
-#right now its testing bc idk how to do this anymore
-
-print('ummmmm')
-
 import os
-Token = os.getenv("token")
-
-print(Token)
-print('okay this works')
-
-
 import discord
 import discord.ext.commands
 
+
+print('hello, world!')
+
+print('script initalized!')
+
+#defines the client and command prefex
 client = discord.ext.commands.Bot(command_prefix ='rc.')
 
 #i have no clue what this does
 @client.event
 
+#bot initlized msg
 async def on_ready():
     print("""
     bot initialized...
     waiting for commands master.
     """)
 
+#test command (more on the way!)
 @client.command()
-
 async def test(ctx):
     await ctx.send('IM ALIVE OH MY GOD IM ALIVE')
+
+#connect bot to discord????????
+Token = os.getenv("token")
+client.run(Token)
