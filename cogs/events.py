6a2e5fe494275e01f249b5c03889e3a83197e71f
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-25 15:31:48 -0600

    tabbed

--- cogs/events.py
+++ cogs/events.py
@@ -8,27 +8,27 @@ class events(discord.ext.commands.Cog):
         self.client = client
     print("events initialized")
 
-#join msg
-@client.event(discord.on_member_join)
-async def on_member_join(member):
+    #join msg
+    @client.event
+    async def on_member_join(member):
 
-    #get username / profile
-    print(member)
-    new_user = discord.Guild.get_member(member)
-    name = new_user.display_name
-    color = new_user.color
+        #get username / profile
+        print(member)
+        new_user = discord.Guild.get_member(member)
+        name = new_user.display_name
+        color = new_user.color
 
-    #this will be removed if it works fine as is
-    print(new_user)
-    print(name)
-    print(color)
+        #this will be removed if it works fine as is
+        print(new_user)
+        print(name)
+        print(color)
 
-    #send welcome msg
-    embed = discord.Embed(title=f"welcome: {name} to **THELAB**", color=color) 
+        #send welcome msg
+        embed = discord.Embed(title=f"welcome: {name} to **THELAB**", color=color) 
 
-    channel = client.get_channel(990364347446460426)
-    await channel.send(embed)
-    
+        channel = client.get_channel(990364347446460426)
+        await channel.send(embed)
+        
 
 def setup(client):
     client.add_cog(events(client))
