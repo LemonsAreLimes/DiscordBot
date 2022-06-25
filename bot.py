f5bd17dae5dce0a26d9a3f776928c0299b7a4745
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-24 20:34:48 -0600

    trying to fix token invalad error

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import discord.ext.commands
 
 print('script initalized!')
 
-Token = str(os.getenv("token"))     #might fix invalad token
+Token = os.getenv("token")     #might fix invalad token
 print(Token)
 print(type(Token))
 
@@ -14,20 +14,18 @@ print('token initalized')
 #defines the client and command prefex
 client = discord.ext.commands.Bot(command_prefix ='rc.')
 
-#i have no clue what this does
-@client.event
-
 #bot initlized msg
+@client.event
 async def on_ready():
     print("""
     bot initialized...
     waiting for commands master.
     """)
 
-#test command (more on the way!)
-@client.command()
-async def test(ctx):
-    await ctx.send('IM ALIVE OH MY GOD IM ALIVE')
+# #test command (more on the way!)
+# @client.command()
+# async def test(ctx):
+#     await ctx.send('IM ALIVE OH MY GOD IM ALIVE')
 
 #connect bot to discord????????
 client.run(Token)
