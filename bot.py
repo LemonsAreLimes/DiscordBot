52eeec7d0614a78e990c0b83ee7fccf1b5c3703a
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-24 21:27:55 -0600

    first steps for reaction roles

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def dexter(ctx): #H E L P
 
 @client.command()
 async def reac(ctx):
-    users = await discord.Message.reactions
+    users = discord.Message.reactions
     await ctx.send(users)
 
 #commands to dev 
