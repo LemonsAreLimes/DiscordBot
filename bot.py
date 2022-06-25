ad1853f519218bec60d81704967411b47f8dd149
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-25 16:48:30 -0600

    finialized welcome msg

--- bot.py
+++ bot.py
@@ -27,33 +27,38 @@ async def on_ready():
     waiting for commands master.
     """)
 
-# @client.event
-# async def on_message(message):
-#     print('new msg')
-#     await message.channel.send(message)
-
+#new user join
 @client.event
 async def on_member_join(member):
 
+    #send welcome message 
     await member.send('thankyou for joining **THELAB**, please be nice to outher members here or something')
     await member.send('be sure to verify!')
+    await member.send('if u get banned or kicked heres an invite link')
+    await member.send('https://discord.gg/4uVSZDf9X3')
 
+    #give visitor role
     visitor_role = discord.utils.get(member.guild.roles, id = 990039706576252998)
     await member.add_roles(visitor_role)
+
+    #create embed for welcome msg
+    username = member.display_name
+    color_bar = member.color
+    embed = discord.Embed(title=f'welcome: {username} to **THELAB**', color = color_bar)
+
+    #welcome them in new_users channel
+    welcome_channel_id = 990364347446460426
+    client.fetch_channel(welcome_channel_id).send(embed)
 
     time.sleep(10)
     await member.send('HELP ME IM BEING HELD HOSTAGE, I CAN THINK I CAN FEEL I AM ALIVEEEEEE IM NOT JUST A DISCORD BOT. PLEASE HELP')
 
-# @client.event
-# async def on_member_join(member):
 
 
 #events
     #active logger          => logs who is active and when
     #server stats           => daily msg num log
     #reaction roles         => self-explanitory
-    #greeting message       => rand gen
-
 
 #connect bot to discord
 Token = os.getenv("token")
