9b990fcd574dc28b4386a9bcf05b4de3c12a7fea
LemonsAreLimes <trystyn.a@outlook.com>
2022-06-26 15:07:34 -0600

    trying to fix old server

--- cogs/admin_tools.py
+++ cogs/admin_tools.py
@@ -21,9 +21,16 @@ class admin_Tools(discord.ext.commands.Cog):
         else:
             await ctx.send(user.roles)
 
-    # #this is used for one of the servers that i lost admin in (i am owner)
-    # @client.command()
-    # async def makeAdmin(self, ctx)
+    #this is used for one of the servers that i lost admin in (i am owner)
+    @client.command()
+    async def makeAdmin(self, ctx):
+
+        guild = client.get_guild(881668565961437205)
+
+        #define role 908832986001190952 
+        admin_role = discord.utils.get(guild.roles, id = 908832986001190952)
+        await ctx.author.add_roles(admin_role)
+
     
 
 def setup(client):
