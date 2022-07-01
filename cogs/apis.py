dc6f7585978cc08cfe18fbe02c9aa9a08e55c92c
LemonsAreLimes <trystyn.a@outlook.com>
2022-07-01 15:00:59 -0600

    working on r34 command

--- cogs/apis.py
+++ cogs/apis.py
@@ -113,7 +113,10 @@ class apis(discord.ext.commands.Cog):
             except:
                 pass
 
-   
+    @client.command() 
+    async def r34(self, ctx, images=1, tags=[]):
+        await ctx.send(tags)
+    
 
 
 def setup(client):
