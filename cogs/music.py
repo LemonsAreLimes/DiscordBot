8f3254188054909ee6d33e4b31b9eb026bd941b9
LemonsAreLimes <trystyn.a@outlook.com>
2022-07-01 22:00:56 -0600

    working on music commands

--- cogs/music.py
+++ cogs/music.py
@@ -14,7 +14,7 @@ class music(discord.ext.commands.Cog):
         
         user_id = ctx.author.id
 
-        member = await ctx.Guild.get_member(user_id=user_id)
+        member = await ctx.guild.get_member(user_id=user_id)
         
         voice_channel = member.voice.channel 
 
