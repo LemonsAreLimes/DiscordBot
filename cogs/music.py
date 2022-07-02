e5a23a4b178a210a71728a88cc4fc24e3972cce6
LemonsAreLimes <trystyn.a@outlook.com>
2022-07-02 15:15:25 -0600

    removed music commands

--- cogs/music.py
+++ cogs/music.py
@@ -1,69 +1,66 @@
-import discord
-import discord.ext.commands
-from youtube_dl import YoutubeDL
-from discord import FFmpegPCMAudio
+# import discord
+# import discord.ext.commands
+# from youtube_dl import YoutubeDL
+# from discord import FFmpegPCMAudio
 
-client = discord.ext.commands.Bot(command_prefix="rc.")
+# client = discord.ext.commands.Bot(command_prefix="rc.")
 
-class music(discord.ext.commands.Cog):
-    def __init__(self, client):
-        self.client = client
-        print("initialized: music")
+# class music(discord.ext.commands.Cog):
+#     def __init__(self, client):
+#         self.client = client
+#         print("initialized: music")
 
-    @client.command()
-    async def join(self, ctx):
+#     @client.command()
+#     async def join(self, ctx):
         
-        #get memeber id from ctx
-        user_id = ctx.author.id
-        member = ctx.guild.get_member(user_id=user_id)
-        voice = member.voice 
+#         #get memeber id from ctx
+#         user_id = ctx.author.id
+#         member = ctx.guild.get_member(user_id=user_id)
+#         voice = member.voice 
 
-        #check if user is in voice channel
-        if voice != None:
-            await ctx.send(f'joining voice channel with {ctx.author}')
-            await voice.channel.connect()
-        else:
-            await ctx.send('join a vc broh')
+#         #check if user is in voice channel
+#         if voice != None:
+#             await ctx.send(f'joining voice channel with {ctx.author}')
+#             await voice.channel.connect()
+#         else:
+#             await ctx.send('join a vc broh')
 
-    @client.command()
-    async def leave(self, ctx):
-        await ctx.voice_client.disconnect()
+#     @client.command()
+#     async def leave(self, ctx):
+#         await ctx.voice_client.disconnect()
 
-    @client.command()
-    async def play(self, ctx, url=None):
-        if url != None:
+#     @client.command()
+#     async def play(self, ctx, url=None):
+#         if url != None:
 
-            #definetly not stolen from stack over flow ;)
-            ydl_opts = {'format': 'bestaudio', 'noplaylist':'True'}
-            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-            voice = ctx.voice_client
+#             #definetly not stolen from stack over flow ;)
+#             ydl_opts = {'format': 'bestaudio', 'noplaylist':'True'}
+#             FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+#             voice = ctx.voice_client
 
-            with YoutubeDL(ydl_opts) as ydl:
-                info = ydl.extract_info(url, download=False)
-            URL = info['formats'][0]['url']
-            voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+#             with YoutubeDL(ydl_opts) as ydl:
+#                 info = ydl.extract_info(url, download=False)
+#             URL = info['formats'][0]['url']
+#             voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
  
-        else:
-            await ctx.send('give us something man')
+#         else:
+#             await ctx.send('give us something man')
 
-    @client.command()
-    async def stop(self, ctx):
-        pass
-        #stops plying the music
+#     @client.command()
+#     async def stop(self, ctx):
+#         pass
+#         #stops plying the music
 
-def setup(client):
-    client.add_cog(music(client))
+# def setup(client):
+#     client.add_cog(music(client))
 
 
 
-#commands to dev 
-    #insult                 => grabs random quote from list
-    #chatbot                => uses either cleverbot or replika idk bc its prolly gonna use headless chromedriver mabey
 
-#music
-    #play       [link], join vc with sender
-    #pause           
-    #stop
-    #skip
-    #join
-    #leave
+# #music
+#     #play       [link], join vc with sender
+#     #pause           
+#     #stop
+#     #skip
+#     #join
+#     #leave
