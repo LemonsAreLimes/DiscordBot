4576179071df1edeec7e128684bd951b71bc44c0
LemonsAreLimes <trystyn.a@outlook.com>
2022-07-05 01:38:54 -0600

    adding message logging for levels

--- bot.py
+++ bot.py
@@ -180,15 +180,13 @@ async def on_raw_reaction_add(payload):
             print('emoji not found')
 
 
-    @client.event
-    async def on_message(payload):
-        
-        guild_id = 989980425529212999
-
-        if payload.guild.id and payload.guild.id == guild_id:
-
-            id = payload.user_id
-            moong.mongo.msgUpdate(id)
+@client.event
+async def on_message(payload):
+    
+    guild_id = 989980425529212999
+    if payload.guild.id and payload.guild.id == guild_id:
+        id = payload.user_id
+        moong.mongo.msgUpdate(id)
 
 
 
