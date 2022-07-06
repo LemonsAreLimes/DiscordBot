129f940e8ffd61c00a58c26e27f45e603f0e1c39
LemonsAreLimes <trystyn.a@outlook.com>
2022-07-05 20:56:10 -0600

    adding games

---
+++ cogs/games.py
@@ -0,0 +1,36 @@
+# import discord
+# import discord.ext.commands
+
+# #game libs (cleans up help command)
+# from src.GAME_coinflp import coinflip
+
+
+# client = discord.ext.commands.Bot(command_prefix="rc.")
+
+# class games(discord.ext.commands.Cog):
+#     def __init__(self, client):
+#         self.client = client
+#         print("initialized: games")
+
+#     @client.command()
+#     async def flip(self, ctx):
+#         coin = coinflip.gen()
+#         print(coin)
+
+#         buttons = [
+#             create_butt
+
+#         ]
+
+
+# def setup(client):
+#     client.add_cog(games(client))
+
+
+
+#games:
+    #coin flip
+    #blackjack
+    #roulette
+    #wordle?
+    #minesweeper?
---
+++ src/GAME_coinflp.py
@@ -0,0 +1,11 @@
+import random
+
+class coinflip:
+
+    def gen():
+        num = random.randint(0,100);
+        evenNum = num % 2
+        return evenNum
+
+    
+
