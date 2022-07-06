9e32cd1e00b2adfedc899cc1e814baa069677025
LemonsAreLimes <trystyn.a@outlook.com>
2022-07-05 21:26:19 -0600

    testing out buttons

--- cogs/games.py
+++ cogs/games.py
@@ -1,34 +1,39 @@
-# import discord
-# import discord.ext.commands
+import discord
+import discord.ext.commands
 
-# # #game libs (cleans up help command)
-# # from src.GAME_coinflp import coinflip
+# #game libs (cleans up help command)
+# from src.GAME_coinflp import coinflip
 
-
-# client = discord.ext.commands.Bot(command_prefix="rc.")
-
-# class games(discord.ext.commands.Cog):
-#     def __init__(self, client):
-#         self.client = client
-#         print("initialized: games")
-
-#     @client.command()
-#     async def MenuTest(self, ctx):
-#         button = 
-
-#     # @client.command()
-#     # async def flip(self, ctx):
-#     #     coin = coinflip.gen()
-#     #     print(coin)
-
-#     #     buttons = [
-#     #         create_butt
-
-#     #     ]
+class View(discord.ui.View):
+    @discord.ui.button(label="test", style=discord.ButtonStyle.primary)
+    async def button_callback(self, button, interaction):
+        await interaction.response.send_message('clicked button')
 
 
-# def setup(client):
-#     client.add_cog(games(client))
+client = discord.ext.commands.Bot(command_prefix="rc.")
+
+class games(discord.ext.commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        print("initialized: games")
+
+    @client.command()
+    async def MenuTest(self, ctx):
+        await ctx.send("okay this part worked", View=View())
+
+    # @client.command()
+    # async def flip(self, ctx):
+    #     coin = coinflip.gen()
+    #     print(coin)
+
+    #     buttons = [
+    #         create_butt
+
+    #     ]
+
+
+def setup(client):
+    client.add_cog(games(client))
 
 
 
