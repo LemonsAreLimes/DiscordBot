e6e76e6b1cd6f52e44888afa2c44ff7e89e2d59c
LemonsAreLimes <trystyn.a@outlook.com>
2022-07-21 16:07:48 -0600

    testing out coin flip game

--- cogs/games.py
+++ cogs/games.py
@@ -1,24 +1,26 @@
-# import discord
-# import discord.ext.commands
+import discord
+import discord.ext.commands
 
-# # #game libs (cleans up help command)
-# from src.GAME_coinflp import coinflip
+# #game libs (cleans up help command)
+from src.GAME_coinflp import cfButtons
 
+client = discord.ext.commands.Bot(command_prefix ='rc.', intents=discord.Intents.all())
 
-# client = discord.ext.commands.Bot(command_prefix ='rc.', intents=discord.Intents.all())
+class games(discord.ext.commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        print("initialized: games")
 
-# class games(discord.ext.commands.Cog):
-#     def __init__(self, client):
-#         self.client = client
-#         print("initialized: games")
+    @client.command()
+    async def cf(self, ctx):
+        view = cfButtons()
+        await ctx.send("heads or tails", view=view)
 
-#     @client.command()
-#     async def gametest(self, ctx):
         
 
 
-# def setup(client):
-#     client.add_cog(games(client))
+def setup(client):
+    client.add_cog(games(client))
 
 
 
--- src/GAME_coinflp.py
+++ src/GAME_coinflp.py
@@ -1,11 +1,36 @@
 import random
+import discord
 
-class coinflip:
+class cfButtons(discord.ui.View):
 
-    def gen():
-        num = random.randint(0,100);
-        evenNum = num % 2
-        return evenNum
+    @discord.ui.button(label="Heads", style=discord.ButtonStyle.primary)
+    async def heads_callback(self, button, interaction):
 
-    
+        for child in self.children: 
+            child.disabled = True
 
+        #gen random coin
+        flip = bool(random.getrandbits(1))
+        if flip:
+            button.style = discord.ButtonStyle.green
+            await interaction.response.edit_message(content="you won!", view=self)
+        else:
+            button.style = discord.ButtonStyle.red
+            await interaction.response.edit_message(content="you lost..", view=self)
+
+    @discord.ui.button(label="Tails", style=discord.ButtonStyle.primary)
+    async def tails_callback(self, button, interaction):
+
+        for child in self.children: 
+            child.disabled = True
+
+        #gen random coin
+        flip = bool(random.getrandbits(1))
+        if flip:
+            button.style = discord.ButtonStyle.green
+            await interaction.response.edit_message(content="you won!", view=self)
+        else:
+            button.style = discord.ButtonStyle.red
+            await interaction.response.edit_message(content="you lost..", view=self)
+
+        
